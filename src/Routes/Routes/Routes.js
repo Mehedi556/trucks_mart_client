06cef0438c2a54744395bb070d6a195ec57ed382
mehedi556 <mehe556di@gmail.com>
2022-11-24 12:15:10 +0600

    adding navbar and footer part

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../../Layout/MainLayout";
+import Blog from "../../Pages/Blog/Blog";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Register from "../../Pages/Register/Register";
+
+export  const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            }
+        ]
+    }
+])
